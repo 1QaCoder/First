4aa5c631d2ab10b917921fea01d7995fd13db9b9
Sharvari <shravarimn@gmail.com>
2022-03-16 23:36:20 -0400

    made changes

---
+++ FirstProject/src/test/java/Assignment/Assignment2.java
@@ -0,0 +1,12 @@
+package Assignment;
+
+public class Assignment2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=2;
+		System.out.println(a);
+
+	}
+
+}
